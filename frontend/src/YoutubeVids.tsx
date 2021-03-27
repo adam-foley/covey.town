4487d3a6a27936a0d1b3b2c1756f38119d41a6d3
jspunna <jspunna214@gmail.com>
2021-03-27 13:52:05 -0400

    created initial/default youtube video collection

---
+++ frontend/src/YoutubeVids.tsx
@@ -0,0 +1,89 @@
+export type YTVideo = {
+    url: string;
+    title: string;
+    channel: string;
+    duration: number;
+}
+
+export const videoList : YTVideo[] = [];
+
+const video1 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=QtXby3twMmI",
+    title: "Coldplay - Adventure Of A Lifetime (Official Video)",
+    channel: "Coldplay",
+    duration: 315,
+};
+videoList.push(video1);
+  
+const video2 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=YqeW9_5kURI",
+    title: "Major Lazer & DJ Snake - Lean On (feat. MØ) (Official Music Video)",
+    channel: "Major Lazer",
+    duration: 179,
+};
+videoList.push(video2);
+
+const video3 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=QtMzV73NAgk",
+    title: "PlayStation 5 Unboxing & Accessories!",
+    channel: "Marques Brownlee",
+    duration: 179,
+};
+videoList.push(video3);
+
+const video4 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=QxGVgXf_LNk",
+    title: "Going Through The Same Drive Thru 1,000 Times",
+    channel: "MrBeast",
+    duration: 937,
+};
+videoList.push(video4);
+
+const video5 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=Kh3RHV5G1Fc",
+    title: "Inside El Chapo’s Escape Tunnel",
+    channel: "Vice News",
+    duration: 678,
+};
+videoList.push(video5);
+
+const video6 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=dllm-HH0toI",
+    title: "Barcelona vs. Paris Saint-Germain: Extended Highlights",
+    channel: "Champions League on CBS Sports",
+    duration: 862,
+};
+videoList.push(video6);
+
+const video7 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=cg1rtWXHSKU",
+    title: "Captain America vs Ultron - Fight Scene - Avengers: Age of Ultron - Movie CLIP HD",
+    channel: "TopMovieClips",
+    duration: 231,
+};
+videoList.push(video7);
+
+const video8 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=kZZj831VbEM",
+    title: "15 Minutes Of Pure Gabriel 'Fluffy' Iglesias Stand-Up",
+    channel: "Netflix Is A Joke",
+    duration: 870,
+};
+videoList.push(video8);
+
+const video9 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=3_9v-7rtVDk",
+    title: "Key & Peele Lose Their Minds Eating Spicy Wings | Hot Ones",
+    channel: "First We Feast",
+    duration: 850,
+};
+videoList.push(video9);
+
+const video10 : YTVideo = {
+    url: "https://www.youtube.com/watch?v=KUwAvIOAHx0",
+    title: "Inside A $387 Million Penthouse In Monaco",
+    channel: "TheRichest",
+    duration: 522,
+};
+videoList.push(video10);
+  
