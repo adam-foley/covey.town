9a10a8bff43e1023c6d174effebb6e8e09bc9b82
adam-foley <adam.f.foley1@gmail.com>
2021-04-13 10:59:56 -0400

    Fixed flaky tests...again

--- services/roomService/src/timer.test.ts
+++ services/roomService/src/timer.test.ts
@@ -3,12 +3,13 @@ import Timer from './timer';
 describe('CoveyTownController', () => {
   let timer : Timer
   let delay : number
-  let createTimeTime : number  
+  let createTimerTime : number  
   beforeEach(() => {
     jest.useFakeTimers();
     delay = 60000
+    createTimerTime = new Date().getTime();
+    Date.now = jest.fn().mockImplementation(()=> createTimerTime);
     timer = new Timer(() => { return true; }, delay);
-    createTimeTime = new Date().getTime();
   });
   afterEach(() =>{
     timer.clearTimer();
@@ -16,13 +17,13 @@ describe('CoveyTownController', () => {
   it('should get elapsed seconds for the current timer when time has elapsed', () => {
     /* Set the time of Date.now to be 35000 milliseconds from when the timer was created
        meaning that there should be 35 seconds elapsed on the timer. */
-    Date.now = jest.fn().mockImplementation(()=> createTimeTime + 35000);
+    Date.now = jest.fn().mockImplementation(()=> createTimerTime + 35000);
     expect(timer.getElapsedSeconds()).toBe(35);
   });
   it('should get elapsed seconds for the current timer when time has not elapsed', () => {
     /* Set the time of Date.now to be equal to when the timer was created
     meaning that there should be o seconds elapsed on the timer. */
-    Date.now = jest.fn().mockImplementation(()=> createTimeTime);
+    Date.now = jest.fn().mockImplementation(()=> createTimerTime + 0);
     expect(timer.getElapsedSeconds()).toBe(0);
   });
   it('should call clearTimeout when clearTimer is called', () => {
